1c10e1953ccd787d48a4bdbc00adb6aec0614f38
shubhendu-repo <rajputshubhendu@gmail.com>
2021-11-22 06:37:02 +0530

    miscellaneous

---
+++ MachineCode/RideSharing/src/main/java/com/models/Vehicle.java
@@ -0,0 +1,31 @@
+package main.java.com.models;
+
+import main.java.com.models.User;
+
+public class Vehicle {
+    private String model;
+    private String license;
+    private User owner;
+
+    public Vehicle(User owner, String model, String license) {
+        this.owner = owner;
+        this.model = model;
+        this.license = license;
+    }
+
+    private User getUser() {
+        return owner;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
